53e16540cfde40ac0e7278cef4fc37411e78fe37
Krystian Kowalski <krystian.kowalski95@gmail.com>
2019-12-07 16:04:48 +0100

    Dodanie metody do przeszukiwania bazy, dodanie projektu testów

---
+++ Zadanie3/UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetProductsByNameTest()
+        {
+            List<Product> products = LinqQueries.GetProductsByName("Flat");
+            foreach(Product product in products)
+            {
+                Console.WriteLine(product.ProductID + " , " + product.Name);
+            }
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3/LinqQueries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class LinqQueries
+    {
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                Table<Product> table = context.GetTable<Product>();
+                List<Product> products = (from product in table
+                                          where product.Name.Contains(namePart)
+                                          select product).ToList();
+                return products;
+            }
+            
+
+        }
+    }
+}
