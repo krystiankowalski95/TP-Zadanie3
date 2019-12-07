435866e8cf0e810725380625510c892f40ecf5c1
Krystian Kowalski <krystian.kowalski95@gmail.com>
2019-12-07 17:05:51 +0100

    Dodanie kolejnych metod realizujących zapytania do bazy danych + testy

--- Zadanie3/UnitTests/UnitTest1.cs
+++ Zadanie3/UnitTests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zadanie3;
 using System.Collections.Generic;
+using System.Data.Linq;
 
 namespace UnitTests
 {
@@ -12,10 +13,42 @@ namespace UnitTests
         public void GetProductsByNameTest()
         {
             List<Product> products = LinqQueries.GetProductsByName("Flat");
-            foreach(Product product in products)
-            {
-                Console.WriteLine(product.ProductID + " , " + product.Name);
-            }
+            Assert.AreEqual(products.Count, 9);
         }
+
+        [TestMethod]
+        public void GetProductsByVendorNameTest()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<Product> products = LinqQueries.GetProductsByVendorName("Beaumont Bikes");
+            Assert.AreEqual(products.Capacity, 4);
+        }
+
+        [TestMethod]
+        public void GetProductsWithNRecentReviewsTest()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<Product> products = LinqQueries.GetProductsWithNRecentReviews(1);
+            Assert.AreEqual(products.Capacity, 4);
+        }
+
+        [TestMethod]
+        public void GetProductVendorByProductNameTest()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            string name = LinqQueries.GetProductVendorByProductName("Flat Washer 1");
+            Assert.AreEqual(name, "Continental Pro Cycles");
+        }
+
+
+        [TestMethod]
+        public void GetProductNamesByVendorNameTest()
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            List<String> productNames = LinqQueries.GetProductNamesByVendorName("Continental Pro Cycles");
+            Assert.AreEqual(productNames.Count, 9);
+        }
+
+        
     }
 }
--- Zadanie3/Zadanie3/LinqQueries.cs
+++ Zadanie3/Zadanie3/LinqQueries.cs
@@ -22,5 +22,53 @@ namespace Zadanie3
             
 
         }
+
+        public static List<Product> GetProductsByVendorName(string vendorName)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                Table<ProductVendor> table = context.GetTable<ProductVendor>();
+                List<Product> products = (from vendor in table
+                                          where vendor.Vendor.Name.Equals(vendorName)
+                                          select vendor.Product).ToList();
+                return products;
+            }
+        }
+
+        public static List<string> GetProductNamesByVendorName(string vendorName)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                Table<ProductVendor> table = context.GetTable<ProductVendor>();
+                List<String> productNames = (from vendor in table
+                                          where vendor.Vendor.Name.Equals(vendorName)
+                                          select vendor.Product.Name).ToList();
+                return productNames;
+            }
+        }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                Table<ProductVendor> table = context.GetTable<ProductVendor>();
+                string name  = (from vendor in table
+                               where vendor.Product.Name.Equals(productName)
+                               select vendor.Vendor.Name).ToList()[0];
+                return name;
+            }
+        }
+
+        public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+            using (DataClasses1DataContext context = new DataClasses1DataContext())
+            {
+                Table<Product> table = context.GetTable<Product>();
+                List<Product> products = (from product in table
+                                          where product.ProductReview.Count.Equals(howManyReviews)
+                                          select product).ToList();
+                return products;
+            }
+        }
     }
 }
